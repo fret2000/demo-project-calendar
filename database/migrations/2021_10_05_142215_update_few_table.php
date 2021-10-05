20a2a4fc3455fb83304ec17aedbfa24258c47f4a
Abaev <abaev@image-spark.ru>
2021-10-05 18:47:56 +0400

    Add migration: set foreign keys for: calendars->user_id, rooms->event_id, events->calendar_id, events->creator_id

---
+++ database/migrations/2021_10_05_142215_update_few_table.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateFewTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table("calendars", function(Blueprint $table)
+        {
+            $table->foreing('user_id')->references('user_id')->on('users')->onDelete('cascade');
+        });
+
+        Schema::table("rooms", function(Blueprint $table)
+        {
+            $table->foreign('event_id')->references('event_id')->on('events')->onDelete('cascade');
+        });
+
+        Schema::table("events", function(Blueprint $table)
+        {
+            $table->foreign('calendar_id')->references('calendar_id')->on('calendars')->onDelete('cascade');
+            $table->foreign('creator_id')->references('user_id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('calendars', function($table)
+        {
+            $table->dropForeign(['user_id']);
+        });
+
+        Schema::table('rooms', function($table)
+        {
+            $table->dropForeign(['event_id']);
+        });
+
+        Schema::table('events', function($table)
+        {
+            $table->dropForeign(['calendar_id']);
+            $table->dropForeign(['creator_id']);
+        });
+    }
+}
