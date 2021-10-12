ac589a5ea81cb57cf47d65c5b8164dacfac0a1af
RubyRascal <lekhasorokin2017@yandex.ru>
2021-10-12 17:38:25 +0300

    routs

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', "App\Http\Controllers\EventController@index");
+Route::get('/', "EventController@index");
 
-Route::post('/create', "App\Http\Controllers\EventController@create");
+Route::post('/create', "EventController@create");
 
-Route::get('/calendar/{idCalendar}/{date}', "App\Http\Controllers\EventController@index");
+Route::get('/calendar/{idCalendar}/{date}', "EventController@index");
 
-Route::get('/worker', "App\Http\Controllers\EventController@indexWorker");
+Route::get('/worker', "EventController@indexWorker");
 
 Route::post('/worker/select', "EventController@select");
 
